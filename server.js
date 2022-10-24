e058d8df32e7eb37fb71b360d0620f9bad091f18
unknown <reinanalves1988@gmail.com>
2022-10-24 17:04:08 -0300

    second

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000; // <== You can change the port
 
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-  })
 server.use(middlewares);
 server.use(router);
 
